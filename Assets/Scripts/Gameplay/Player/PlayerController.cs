6964b2f197f4a998cb34c39ef4dc8d0ae06caa4f
ranvi7150 <ranvi7150@gmail.com>
2026-04-20 14:03:33 +0900

    Add basic player movement and jump
    Implement player controller with movement, jump, and ground check

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+namespace Icarus.Gameplay.PlayerController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerController : MonoBehaviour
     {
-        
-    }
+        [Header("Movement")]
+        [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private float jumpForce = 5f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [Header("Ground Check")]
+        [SerializeField] private Collider2D feetCollider;
+        [SerializeField] private LayerMask groundLayers = ~0;
+
+        private Rigidbody2D _rb;
+        private Vector2 _moveInput;
+        private bool _jumpQueued;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void FixedUpdate()
+        {
+            //MOVE
+            _rb.linearVelocity = new Vector2(_moveInput.x * moveSpeed, _rb.linearVelocity.y);
+
+            //JUMP
+            if (_jumpQueued)
+            {
+                _jumpQueued = false;
+                _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
+            }
+        }
+
+        private bool IsGrounded()
+        {
+            return feetCollider.IsTouchingLayers(groundLayers);
+        }
+
+
+        /* Player Control Event */
+
+        public void Move(InputAction.CallbackContext ctx)
+        {
+            _moveInput = ctx.ReadValue<Vector2>();
+        }
+
+        public void Jump(InputAction.CallbackContext ctx)
+        {
+            if (ctx.performed && IsGrounded())
+            {
+                _jumpQueued = true;
+            }
+        }
+
     }
 }
