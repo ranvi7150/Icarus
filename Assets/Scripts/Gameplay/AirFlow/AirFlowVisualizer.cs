916f0c36ee528f062d2cc29883bde9e639e35f05
ranvi7150 <ranvi7150@gmail.com>
2026-04-24 10:47:01 +0900

    Simplify airflow direction visualization
    visualizer is a code that dont have to use yet

--- Assets/Scripts/Gameplay/AirFlow/AirFlowVisualizer.cs
+++ Assets/Scripts/Gameplay/AirFlow/AirFlowVisualizer.cs
@@ -2,40 +2,8 @@
 
 namespace Icarus.Gameplay.AirFlow
 {
-    [ExecuteAlways]
     public class AirFlowVisualizer : MonoBehaviour
     {
-        [Header("References")]
-        [SerializeField] private AirFlowZone airFlowZone;
-
-        private void Reset()
-        {
-            if (airFlowZone == null)
-            {
-                airFlowZone = GetComponentInParent<AirFlowZone>();
-            }
-        }
-
-        private void OnValidate()
-        {
-            UpdateArrowRotation();
-        }
-
-        private void LateUpdate()
-        {
-            UpdateArrowRotation();
-        }
-
-        private void UpdateArrowRotation()
-        {
-            if (airFlowZone == null)
-            {
-                return;
-            }
-
-            Vector2 direction = airFlowZone.AirFlowDirection;
-            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, 0f, angle);
-        }
+        
     }
 }
